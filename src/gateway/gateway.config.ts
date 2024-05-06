820fa2926d6beecc9a318bd14da050cc99ac1b90
BrunoGazineu <107951111+BrunoGazineu@users.noreply.github.com>
2024-05-06 18:53:10 -0300

    feat: gateway configuration types

--- src/gateway/gateway.config.ts
+++ src/gateway/gateway.config.ts
@@ -3,8 +3,23 @@ const fs = require('fs');
 
 
 interface GatewaySettings {
-    clusters: [],
-    routes: []
+    clusters: {
+        [clusterName: string]: {
+            destinations: string[]
+        }
+    },
+    routes: {
+        [routeName: string]: {
+            clusterName: string,
+            match: {
+                path: string,
+                methods: string[];
+            },
+            transforms?: {
+                pathPattern: string
+            }[]
+        }
+    }
 }
 
 @Injectable()
@@ -15,8 +30,10 @@ export class GatewayConfiguration {
     private readonly gatewaySettingsBaseFileExtension = 'json';
 
     constructor() {
-        if (!this.gatewaySettings)
+        if (!this.gatewaySettings) {
             this.gatewaySettings = this.loadSettings();
+            this.settingsHealth();
+        }
     }
 
     private getSettingsFileName(environment?: string) {
@@ -27,5 +44,15 @@ export class GatewayConfiguration {
         const defaultSettings = JSON.parse(fs.readFileSync(this.getSettingsFileName(), 'utf-8'));
         const environmentSettings = JSON.parse(fs.readFileSync(this.getSettingsFileName(process.env.NODE_ENV), 'utf-8'));
         return { ...defaultSettings, ...environmentSettings } as GatewaySettings;
+    }
+
+    private settingsHealth() : void {
+        Object.entries(this.gatewaySettings.routes).forEach(([route, value]) => {
+            if (!value.clusterName) throw new Error(`ROUTE ${route} NÃO POSSUI CLUSTER ATRELADO`)
+            if (!(value.clusterName in this.gatewaySettings.clusters)) {
+                throw new Error(`ROUTE ${route} CLUSTER NÃO EXISTE EM CLUSTERS`)
+            }
+            if (!value.match) throw new Error(`ROUTE ${route} NÃO POSSUI MATCH CONFIGURADO`)
+        })
     }
 }
